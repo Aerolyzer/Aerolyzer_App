357e6a54256c9755cc268e1b9b24c78fe86c956c
Reilly Collins <colliell@onid.orst.edu>
2017-03-08 18:29:44 -0800

    Working, image and gallery indexes accessible via search url

--- Aerolyzer/app/models.py
+++ Aerolyzer/app/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Images(models.Model):
+class Image(models.Model):
 	imageId = models.AutoField(primary_key=True)
 	exif = models.CharField(max_length=200)
 	misr = models.CharField(max_length=200)
@@ -14,7 +14,7 @@ class Images(models.Model):
 		return self.results
 
 class Gallery(models.Model):
-	imageId = models.ForeignKey(Images)
+	imageId = models.ForeignKey(Image)
 	username = models.CharField(max_length=30)
 
 	def __unicode__(self):
--- Aerolyzer/app/search_indexes.py
+++ Aerolyzer/app/search_indexes.py
@@ -1,19 +1,18 @@
-import datetime
 from haystack import indexes
-from app.models import Images, Gallery
+from app.models import Image, Gallery
 
-class ImagesIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
+class ImageIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
     exifIdx = indexes.CharField(model_attr='exif')
     misrIdx = indexes.CharField(model_attr='misr')
     wunderIdx = indexes.CharField(model_attr='wunder')
     resultsIdx = indexes.CharField(model_attr='results')
 
     def get_model(self):
-        return Images
+        return Image
 
 class GalleryIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
+    text = indexes.CharField(document=True, use_template=True)
     imageIdIdx = indexes.IntegerField(model_attr='imageId')
     usernameIdx = indexes.CharField(model_attr='username')
 
